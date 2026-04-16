5f7923ae25eb79f53163ddbc6bf1533ded83b546
edoardoFerrini <edoardo.ferrini@gmail.com>
2026-04-16 15:56:20 +0200

    creati island astro

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+export default new Map([
+["src/content/posts/astro-per-blog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fastro-per-blog.mdx&astroContentModuleFlag=true")]]);
+		
