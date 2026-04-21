08b44478cc115113a80a0dcc26b6235e67a99203
edoardoFerrini <edoardo.ferrini@gmail.com>
2026-04-21 09:02:26 +0200

    aggiunto pacchetto astrofind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import pagefind from 'astro-pagefind';
 
 export default defineConfig({
   site: 'https://edoardoferrini.github.io',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), pagefind()],
 });
